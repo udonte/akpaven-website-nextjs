057d10c416128fff052ac1a7c8d5b0d1835cd2ff
George Udonte <georgeudonte@gmail.com>
2026-07-14 22:31:52 +0100

    fixed error

--- src/components/home/ImageSection.tsx
+++ src/components/home/ImageSection.tsx
@@ -1,17 +1,15 @@
-﻿import React from " react\;
-
-type ImageItem = {
- src: string;
- alt?: string;
- overlayText?: string;
+﻿type ImageItem = {
+  src: string;
+  alt?: string;
+  overlayText?: string;
 };
 
 interface ImageSectionProps {
- /**
- * Array of images to display. Provide one item for a single full‑width image,
- * or two items for a side‑by‑side layout.
- */
- images: ImageItem[];
+  /**
+   * Array of images to display. Provide one item for a single full‑width image,
+   * or two items for a side‑by‑side layout.
+   */
+  images: ImageItem[];
 }
 
 /**
@@ -20,26 +18,26 @@ interface ImageSectionProps {
  * positioned at the bottom‑left corner of the image.
  */
 export default function ImageSection({ images }: ImageSectionProps) {
- const displayed = images.slice(0, 2);
- const containerClass = displayed.length === 1 ? \w-full\ : \flex gap-4\;
+  const displayed = images.slice(0, 2);
+  const containerClass = displayed.length === 1 ? "w-full" : "flex gap-4";
 
- return (
- <div className={containerClass}>
- {displayed.map((img, idx) => (
- <div key={idx} className=\relative w-full\>
- {/* eslint-disable-next-line @next/next/no-img-element */}
- <img
- src={img.src}
- alt={img.alt ?? \\}
- className=\w-full h-auto object-cover rounded-lg\
- />
- {img.overlayText && (
- <div className=\absolute bottom-0 left-0 bg-gradient-to-t from-black/70 to-transparent text-white p-4 rounded-b-lg\>
- {img.overlayText}
- </div>
- )}
- </div>
- ))}
- </div>
- );
+  return (
+    <div className={containerClass}>
+      {displayed.map((img, idx) => (
+        <div key={idx} className="relative w-full">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src={img.src}
+            alt={img.alt ?? ""}
+            className="w-full h-auto object-cover rounded-lg"
+          />
+          {img.overlayText && (
+            <div className="absolute bottom-0 left-0 bg-linear-to-t from-black/70 to-transparent text-white p-4 rounded-b-lg">
+              {img.overlayText}
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
 }
